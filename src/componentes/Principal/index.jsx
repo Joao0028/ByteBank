995d10db52c802b7601878d872e04429a86abf92
Joao0028 <joaopaulonascimento720@gmail.com>
2023-10-06 20:47:24 -0300

    usando o prettier para formatar o código

--- src/componentes/Principal/index.jsx
+++ src/componentes/Principal/index.jsx
@@ -1,28 +1,28 @@
-import React from 'react';
-import estilos from './Principal.module.css';
-import { ReactComponent as Ilustracao } from './ilustracao.svg';
-import Saldo from './Saldo';
+import React from "react";
+import estilos from "./Principal.module.css";
+import { ReactComponent as Ilustracao } from "./ilustracao.svg";
+import Saldo from "./Saldo";
 
 const data = Date.now();
 const hoje = new Date(data);
 const diasDaSemana = [
-  'Domingo',
-  'Segunda-feira',
-  'Terça-feira',
-  'Quarta-feira',
-  'Quinta-feira',
-  'Sexta-feira',
-  'Sábado',
+  "Domingo",
+  "Segunda-feira",
+  "Terça-feira",
+  "Quarta-feira",
+  "Quinta-feira",
+  "Sexta-feira",
+  "Sábado",
 ];
 
 export default function Principal({ saldo }) {
   return (
     <section className={estilos.container}>
       <div className={estilos.detalhe__superior} />
-      <h1 className={estilos.titulo}>Olá, Joana :)!</h1>
+      <h1 className={estilos.titulo}>Olá, João :)!</h1>
       <p className={estilos.data}>{`${
         diasDaSemana[hoje.getDay()]
-      }, ${hoje.toLocaleDateString('pt-BR')}`}</p>
+      }, ${hoje.toLocaleDateString("pt-BR")}`}</p>
       <div className={estilos.wrapper}>
         <Ilustracao className={estilos.ilustracao} width="230" height="185" />
         <Saldo saldo={saldo} />
